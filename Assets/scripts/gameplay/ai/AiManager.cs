be6add085add8dace12d1c5e46e3db87331bcc04
JMYang <jaewonmichael@gmail.com>
2017-03-06 16:12:48 -0800

    Starting

---
+++ Assets/scripts/gameplay/ai/AiManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiManager : MonoBehaviour
+{	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 steering = Vector2.zero;
+        steering += AiSeek.GetSteeringForce(this.transform);
+        CommandHandler.Instance.AddCommands(new AccelerateCommand(this.gameObject, steering.x, steering.y));
+	}
+}
+
+/// <summary>
+/// Base class for AI behaviors
+/// </summary>
+public abstract class AiBehavior
+{
+    /// <summary>
+    /// Function to get result steering force
+    /// </summary>
+    /// <returns>Returns the force to use</returns>
+    public abstract Vector2 GetSteeringForce(Transform target);
+}
+
+#region Behaviors
+
+public class AiSeek : AiBehavior
+{
+    public static override Vector2 GetSteeringForce(Transform target)
+    {
+        return Vector2.zero;
+    }
+}
+
+#endregion Behaviors
